1289be642f74767b1a40e94cc51469538215718d
c0r3dump <c0r3dump@arch.com>
2024-06-14 00:45:25 +0800

    add more ble tools

--- software_list.sh
+++ software_list.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 pacman -Qqen > personal_install.txt
-history > zsh_history
+echo "--------yay--------" >> personal_install.txt
+yay -Qm >> personal_install.txt
+# history > zsh_history
 cat ~/.zsh_history > c0r3dump_history
 # sudo history > root_history
 sudo cat /root/.bash_history > root_history
+./zsh_history.exp
